1b1730298e4cc0c05fb143cdd94f48fa82c4d3e4
Sorabh-Kumar <77542001+Sorabh-Kumar@users.noreply.github.com>
2024-04-19 22:44:28 +0530

    Add files via upload

---
+++ vendor.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div id="main">
+                <H1>Welcome<br> Vendor</H1>
+                <div id="vendorbtn">
+                    <button id="vendorpagebtn">Your item</button>
+                    <button id="vendorpagebtn"><a href="add-item.php">Add new item</a></button>
+                    <button id="vendorpagebtn">Transaction</button>
+                    <button id="vendorpagebtn">Logout</button>
+                </div>
+                
+
+
+  
+    </div>
+
+    
+</body>
+</html>
